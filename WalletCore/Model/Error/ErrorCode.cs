f74631a33aab86e975f9b3fc102b1ba10073cf14
Alan Guilherme Mees <alan.mees@linx.com.br>
2022-02-02 01:53:38 -0300

    Base Error Model

---
+++ WalletCore/Model/Error/ErrorCode.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace WalletCore.Model.Error
+{
+    public enum ErrorCode
+    {
+        [Description("Carteira não encontrada")]
+        WalletNotFound = 1,
+
+        [Description("Saldo insuficiente")]
+        InsufficientFunds = 2
+    }
+}
---
+++ WalletCore/Model/Error/ErrorResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WalletCore.Extension;
+
+namespace WalletCore.Model.Error
+{
+    public class ErrorResponse
+    {
+        public string Message { get; private set; }
+
+        public int? ErrorCode { get; private set; }
+
+        public bool HasError { get => !String.IsNullOrEmpty(Message); }
+
+        public ErrorResponse(ErrorCode error)
+        {
+            ErrorCode = ((int)error);
+            Message = error.GetEnumDescription();
+        }
+
+        protected ErrorResponse()
+        {}
+    }
+}
