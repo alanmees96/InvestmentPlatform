d62d107dc6348fd29d94be2a2f96617e7aac8de6
Alan Guilherme Mees <alan.mees@linx.com.br>
2022-02-03 01:50:05 -0300

    Clean and reorganize

--- WalletCore/Model/Database/Wallet.cs
+++ WalletCore/Model/Database/Wallet.cs
@@ -6,25 +6,23 @@ namespace WalletCore.Model.Database
 {
     public class Wallet
     {
+        private double _moneyAvailable;
+        private double _moneyInvested;
         public ObjectId _id { get; set; }
 
+        public double MoneyAvailable
+        {
+            get => _moneyAvailable;
+            set => _moneyAvailable = value.CurrencyRound();
+        }
+
+        public double MoneyInvested
+        {
+            get => _moneyInvested;
+            set => _moneyInvested = value.CurrencyRound();
+        }
+
         public Owner Owner { get; set; }
-
-        private double _moneyAvailable;
-
-        public double MoneyAvailable 
-        { 
-            get => _moneyAvailable; 
-            set => _moneyAvailable = value.CurrencyRound(); 
-        }
-
-        private double _moneyInvested;
-        public double MoneyInvested 
-        { 
-            get => _moneyInvested; 
-            set => _moneyInvested = value.CurrencyRound(); 
-        }
-
         public List<Share> Shares { get; set; }
     }
 }
--- WalletCore/Model/ShareBase.cs
+++ WalletCore/Model/ShareBase.cs
@@ -4,22 +4,24 @@ namespace WalletCore.Model
 {
     public class ShareBase
     {
-        public string Symbol { get; set; }
-
-        public int Quantity { get; set; }
         private double _avgPurchasePrice;
-        public double AVGPurchasePrice 
-        { 
-            get => _avgPurchasePrice; 
-            set => _avgPurchasePrice = value.CurrencyRound(); 
+        public double AVGPurchasePrice
+        {
+            get => _avgPurchasePrice;
+            set => _avgPurchasePrice = value.CurrencyRound();
         }
 
+        public int Quantity { get; set; }
         public ShareBase(ShareBase share)
         {
             Symbol = share.Symbol;
             Quantity = share.Quantity;
             AVGPurchasePrice = share.AVGPurchasePrice;
         }
-        public ShareBase() {}
+
+        public ShareBase()
+        { }
+
+        public string Symbol { get; set; }
     }
 }
